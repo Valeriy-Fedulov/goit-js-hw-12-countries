0fec7bc5e1fe7927609451b734ebf93186f68b78
vofm <valerafm.vofm@gmail.com>
2021-09-19 21:51:36 +0300

    Create all search

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,30 +1,53 @@
 import {fetchCountries} from "./fetchCountries";
 import templateCountries from "../template/countries.hbs";
 import templateCountry from "../template/country.hbs";
+import {error, defaults, defaultStack} from '../../node_modules/@pnotify/core/dist/PNotify.js';
+
+const debounce = require('lodash.debounce');
+
+defaults.mode = 'light';
 
 const listCountriesRef = document.querySelector(".countries");
-const inputListRef = document.querySelector(".inputList");
+const getCountryRef = document.querySelector(".country");
+const inputListRef = document.querySelector(".inputlist");
 
-inputListRef.addEventListener('input', fetchCountries(inputListRef.value));
+inputListRef.addEventListener('input', debounce(onInputSearche, 500));
 
-fetchCountries(item)
-  .then(country => {
-    if (country.length > 1) {
-      const markup = country
-        .map((count) => {
-          return templateCountry(count);
-        })
-    
-      listCountriesRef.innerHTML = markup;
-    }else {
-      const markup = country
-        .map((count) => {
-          return templateCountries(count);
-        })
-    
-      listCountriesRef.innerHTML = markup;
-    }
+function onInputSearche(e) {
+  listCountriesRef.innerHTML = '';
+  getCountryRef.innerHTML = '';
+  fetchCountries(e.target.value)
+    .then(country => {
+      defaultStack.close();
+      if (country.length > 10) {
+        error({
+          text: 'Too many matches found. Please enter a more specific query!'
+        });
+      } else {
+        if (country.length > 1) {
+          const markup = country
+            .map((count) => {
+              return templateCountries(count);
+            })
+            .join("");
+          listCountriesRef.innerHTML = markup;
+        } else {
+          const markup = country
+            .map((count) => {
+              return templateCountry(count);
+            })
+          getCountryRef.innerHTML = markup;
+        }
+      }
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch(errors => {
+      error({
+          text: 'Invalid characters entered. Please enter a country name!'
+        });
+    })  
+};
+
+
+
+  
+  
